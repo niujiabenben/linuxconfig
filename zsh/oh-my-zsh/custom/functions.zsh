170214769df894a4a31f264e530e593979ad6e61
chenli <chen220li@163.com>
2021-05-28 18:18:37 +0800

    update functions.zsh

--- zsh/oh-my-zsh/custom/functions.zsh
+++ zsh/oh-my-zsh/custom/functions.zsh
@@ -1,56 +1,76 @@
 #! /bin/zsh
 
-### start emacs server if no emacs server is running
+EMACSEXE=${HOME}/Documents/tools/emacs/bin/emacs
+CLIENTEXE=${HOME}/Documents/tools/emacs/bin/emacsclient
+
+# start emacs server if no emacs server is running
 startemacs() {
-    local NUM=`ps xuf | grep 'emacs --daemon' | wc -l`
-    if [ $NUM -le 1 ]; then
-        emacs --daemon
-    else
+    if [ $(ps xuf | grep emacs | wc -l) -gt 1 ]; then
         echo "emacs server is already started."
+    else
+        ${EMACSEXE} --daemon
     fi
 }
 
-### kill emacs server if emacs server is running
+# kill emacs server if emacs server is running
 killemacs() {
-    local NUM=`ps xuf | grep 'emacs --daemon' | wc -l`
-    if [ $NUM -le 1 ]; then
+    if [ $(ps xuf | grep emacs | wc -l) -le 1 ]; then
         echo "emacs server is already killed."
     else
-        emacsclient -e '(kill-emacs)'
+        ps xuf | grep emacs | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+   fi
+}
+
+# open file in emacsclient mode
+openfile() {
+    name=$1
+    mode=$2
+    if [ ${name} = nil ]; then
+        ${CLIENTEXE} -t --eval "(emacs-client-init-find-file nil ${mode})"
+    elif [ -f ${name} ]; then
+        name=$(realpath ${name})
+        ${CLIENTEXE} -t --eval "(emacs-client-init-find-file \"${name}\" ${mode})"
+    else
+        echo "${name} is not a file."
     fi
 }
 
-### open file while checking if it exists
+# open file in emacsclient mode
 e() {
     if [ $# -eq 0 ]; then
-        emacsclient -t
+        openfile nil -1
     elif [ $# -eq 1 ]; then
-        if [ -f $1 ]; then
-            emacsclient -t --eval "(emacs-client-init-find-file \"$1\" -1)"
-        else
-            echo "$1 is not a file."
-        fi
+        openfile $1 -1
     else
         echo "command e requires only one input."
     fi
 }
 
-### open file with read only mode
+# open file with read only mode
 ev() {
     if [ $# -eq 0 ]; then
-        emacsclient -t
+        openfile nil 1
     elif [ $# -eq 1 ]; then
-        if [ -f $1 ]; then
-            emacsclient -t --eval "(emacs-client-init-find-file \"$1\" t)"
-        else
-            echo "$1 is not a file."
-        fi
+        openfile $1 1
     else
         echo "command ev requires only one input."
     fi
 }
 
-### avoid unintentional removal
+# open file directly by emacs
+emacs() {
+    if [ $# -eq 0 ]; then
+        ${EMACSEXE} --eval "(emacs-client-init-find-file nil -1)"
+    elif [ $# -gt 1 ]; then
+        echo "command emacs requires only one input."
+    elif [ -f $1 ]; then
+        ${EMACSEXE} --eval "(emacs-client-init-find-file \"$1\" -1)"
+    else
+        echo "$1 is not a file."
+    fi
+}
+
+# avoid unintentional removal
 rm() {
     realpwd=$(realpath "${PWD}")
     recycle=$(realpath "${HOME}/.recycle")
